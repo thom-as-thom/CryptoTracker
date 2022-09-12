693f9fdc6e5c0f553c67f1f5a21ff5c2ccefcf6d
Thomas <Thomaskurz.15@gmail.com>
2022-09-12 13:41:55 -0300

    fixed typing errors

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,12 @@
+export type Market_data = {
+  price_usd: number;
+  percent_change_usd_last_24_hours: number;
+};
+
+export type Metrics = {
+  market_data: Market_data;
+};
+
 export type CoinData = {
   name: string;
   symbol: string;
@@ -5,9 +14,8 @@ export type CoinData = {
   percent_change_usd_last_24_hours: number;
   id: string;
   imgUrl: string;
-  data: object;
-  metrics: object;
-  market_data: number[];
+  metrics: Metrics;
+  market_data: Market_data;
 };
 
 export type Props = {
