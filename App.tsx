dfdace438f547f4fb34bc58cfb2f76de91c3c0c9
Thomas <Thomaskurz.15@gmail.com>
2022-09-14 01:36:32 -0300

    added navigation and store

--- App.tsx
+++ App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import Home from './src/components/Home';
+import Home from './src/screens/Home';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {AddCurrency} from './src/screens/AddCrypto';
+import store from './src/store';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return <Home />;
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="AddCurrency" component={AddCurrency} />
+          <Stack.Screen name="home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
 };
 
 export default App;
