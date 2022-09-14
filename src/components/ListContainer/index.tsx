1c387025c0a970b518305f9cdf6353f8db2d1cdf
Thomas <Thomaskurz.15@gmail.com>
2022-09-14 01:40:21 -0300

    implemented redux

--- src/components/ListContainer/index.tsx
+++ src/components/ListContainer/index.tsx
@@ -1,38 +1,34 @@
-import React, {ReactElement, useEffect, useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import React, {ReactElement} from 'react';
 import {FlatList, SafeAreaView} from 'react-native';
+import {useSelector} from 'react-redux';
 import Button from '../Button';
 import Cryptoview from '../CryptoView';
 import Header from '../Header';
 
 const ListContainer = (): ReactElement => {
-  const [Cryptos, setCryptos] = useState([]);
-
-  useEffect(() => {
-    async () => {
-      try {
-        const resp = await fetch(
-          'https://data.messari.io/api/v2/assets?fields=id,name,slug,symbol,metrics/market_data',
-        );
-        const ResJson = await resp.json();
-        setCryptos(ResJson.data);
-      } catch (err) {
-        console.log(err);
-      } finally {
-        console.log(Cryptos);
-      }
-    };
-  }, [Cryptos]);
-
+  const Cryptos = useSelector(state => state.addedCryptos);
+  const criptos = Cryptos.addedCryptos;
+  const navigation = useNavigation();
   const renderItem = ({item}) => <Cryptoview coin={item} />;
+  const onClick = () => {
+    navigation.navigate('AddCurrency');
+  };
 
   return (
     <SafeAreaView>
       <FlatList
         ListHeaderComponent={<Header />}
-        data={Cryptos}
-        keyExtractor={item => item.id}
+        data={criptos}
+        keyExtractor={item => item.market_data.id}
         renderItem={renderItem}
-        ListFooterComponent={<Button text="+ Add a cryptocurrency" />}
+        ListFooterComponent={
+          <Button
+            text="+Add a Cryptocurrency"
+            onClick={onClick}
+            type="navigation"
+          />
+        }
       />
     </SafeAreaView>
   );
