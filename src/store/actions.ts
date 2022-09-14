f26b49c131a061a6fb2bb07c2f645cc09f1a7737
Thomas <Thomaskurz.15@gmail.com>
2022-09-14 20:15:57 -0300

    fixed 'AddCrypto' function and implemented 'DeleteCrypto'

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,15 +1,35 @@
 export const ADD_CRYPTO = 'ADD_CRYPTO';
+export const DELETE_CRYPTO = 'DELETE_CRYPTO';
 
-export const addCrypto = Key => {
+export const addCrypto = (Key, addedCryptos) => {
   return async dispatch => {
     try {
       const resp = await fetch(
         `https://data.messari.io/api/v1/assets/${Key}/metrics/market-data`,
       );
-      const ResJson = await resp.json();
-      dispatch({type: ADD_CRYPTO, payload: ResJson.data});
+      if (resp.ok) {
+        const ResJson = await resp.json();
+        const Found = addedCryptos.filter(
+          item => item.Asset.id === ResJson.data.Asset.id,
+        );
+        if (Found.length > 0) {
+          alert(`${Found[0].Asset.name} is already in your list.`);
+        } else {
+          dispatch({type: ADD_CRYPTO, payload: ResJson.data});
+        }
+      } else {
+        alert(`We cant find ${Key}, are you sure it's spelled this way?`);
+      }
     } catch (err) {
       console.log(err);
     }
+  };
+};
+
+export const deleteCrypto: Function = (Key, Cryptos) => {
+  const NewList = Cryptos.filter(item => item.Asset.id !== Key);
+  console.log(Cryptos);
+  return dispatch => {
+    dispatch({type: DELETE_CRYPTO, payload: NewList});
   };
 };
--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,4 +1,4 @@
-import {ADD_CRYPTO} from './actions';
+import {ADD_CRYPTO, DELETE_CRYPTO} from './actions';
 
 const initialState = {
   addedCryptos: [],
@@ -9,6 +9,11 @@ export default (state = initialState, action) => {
     return {
       ...state,
       addedCryptos: [...state.addedCryptos, action.payload],
+    };
+  } else if (action.type === DELETE_CRYPTO) {
+    return {
+      ...state,
+      addedCryptos: action.payload,
     };
   } else {
     return state;
