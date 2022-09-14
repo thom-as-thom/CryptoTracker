070db0b268061ff39fa5ceec6f3b990de7dca8a6
Thomas <Thomaskurz.15@gmail.com>
2022-09-14 20:13:09 -0300

    changed endpoint and object manipulation

--- src/components/ListContainer/index.tsx
+++ src/components/ListContainer/index.tsx
@@ -1,26 +1,24 @@
 import {useNavigation} from '@react-navigation/native';
-import React, {ReactElement} from 'react';
+import React from 'react';
 import {FlatList, SafeAreaView} from 'react-native';
 import {useSelector} from 'react-redux';
 import Button from '../Button';
 import Cryptoview from '../CryptoView';
 import Header from '../Header';
 
-const ListContainer = (): ReactElement => {
-  const Cryptos = useSelector(state => state.addedCryptos);
-  const criptos = Cryptos.addedCryptos;
+const ListContainer = (): JSX.Element => {
+  const Cryptos = useSelector(state => state.addedCryptos.addedCryptos);
   const navigation = useNavigation();
   const renderItem = ({item}) => <Cryptoview coin={item} />;
   const onClick = () => {
-    navigation.navigate('AddCurrency');
+    navigation.navigate(AddCurrency);
   };
-
   return (
     <SafeAreaView>
       <FlatList
         ListHeaderComponent={<Header />}
-        data={criptos}
-        keyExtractor={item => item.market_data.id}
+        data={Cryptos}
+        keyExtractor={item => item.Asset.id}
         renderItem={renderItem}
         ListFooterComponent={
           <Button
@@ -33,5 +31,4 @@ const ListContainer = (): ReactElement => {
     </SafeAreaView>
   );
 };
-
 export default ListContainer;
