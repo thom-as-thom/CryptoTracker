6711e3fa07f5139c52b9ab73c021bb6f7f013952
Thomas <Thomaskurz.15@gmail.com>
2022-09-14 20:10:38 -0300

    changed API endpoint and implemented 'deleteCoin'

--- src/components/CryptoView/index.tsx
+++ src/components/CryptoView/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Pressable, Text} from 'react-native';
 import TinyImage from '../TinyImage';
 import theme from '../../theme';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
@@ -13,46 +13,63 @@ import {
   Change,
   ChangePercentage,
 } from './styles';
+import {deleteCrypto} from '../../store/actions';
+import {useDispatch, useSelector} from 'react-redux';
 
-const Cryptoview = ({coin}: {coin: CoinData}): JSX.Element => (
-  <CryptoContainer>
-    <NameView>
-      <TinyImage
-        imgUrl={`https://messari.io/asset-images/${coin.Asset.id}/128.png`}
-      />
-      <CryptoName>
-        <Bold> {coin.Asset.name} </Bold>
-        <Text> {coin.Asset.symbol} </Text>
-      </CryptoName>
-    </NameView>
-    <StatsView>
-      <Bold> ${coin.market_data.price_usd.toFixed(2)} </Bold>
-
-      <Change>
-        {coin.market_data.percent_change_usd_last_24_hours > 0 ? (
-          <MaterialIcon name="call-made" size={18} color={theme.colors.green} />
-        ) : (
-          <MaterialIcon
-            name="call-received"
-            size={18}
-            color={theme.colors.red}
+const Cryptoview = ({coin}: {coin: CoinData}): JSX.Element => {
+  const dispatch = useDispatch();
+  const Cryptos: CoinData = useSelector(
+    state => state.addedCryptos.addedCryptos,
+  );
+  const DeleteCrypto = () => {
+    dispatch(deleteCrypto(coin.Asset.id, Cryptos));
+  };
+  return (
+    <Pressable onPress={DeleteCrypto}>
+      <CryptoContainer>
+        <NameView>
+          <TinyImage
+            imgUrl={`https://messari.io/asset-images/${coin.Asset.id}/128.png`}
           />
-        )}
+          <CryptoName>
+            <Bold> {coin.Asset.name} </Bold>
+            <Text> {coin.Asset.symbol} </Text>
+          </CryptoName>
+        </NameView>
+        <StatsView>
+          <Bold> ${coin.market_data.price_usd.toFixed(2)} </Bold>
 
-        <ChangePercentage
-          inputColor={
-            coin.market_data.percent_change_usd_last_24_hours > 0
-              ? theme.colors.green
-              : theme.colors.red
-          }>
-          {Math.abs(
-            coin.market_data.percent_change_usd_last_24_hours.toFixed(2),
-          )}
-          %
-        </ChangePercentage>
-      </Change>
-    </StatsView>
-  </CryptoContainer>
-);
+          <Change>
+            {coin.market_data.percent_change_usd_last_24_hours > 0 ? (
+              <MaterialIcon
+                name="call-made"
+                size={18}
+                color={theme.colors.green}
+              />
+            ) : (
+              <MaterialIcon
+                name="call-received"
+                size={18}
+                color={theme.colors.red}
+              />
+            )}
+
+            <ChangePercentage
+              inputColor={
+                coin.market_data.percent_change_usd_last_24_hours > 0
+                  ? theme.colors.green
+                  : theme.colors.red
+              }>
+              {Math.abs(
+                coin.market_data.percent_change_usd_last_24_hours.toFixed(2),
+              )}
+              %
+            </ChangePercentage>
+          </Change>
+        </StatsView>
+      </CryptoContainer>
+    </Pressable>
+  );
+};
 
 export default Cryptoview;
